fdab4fd061dd62e8c392eef69602ba9361f4da2e
Brian Niski <204236460+niskbcodepath@users.noreply.github.com>
2025-07-29 12:30:05 -0400

    Week 8

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ let themeButton = document.getElementById("theme-button");
 // Step 2: Write the callback function
 const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
+    // Change --bg-color based on dark mode state
+    if (document.body.classList.contains("dark-mode")) {
+        document.documentElement.style.setProperty('--bg-color', '#3e3377');
+    } else {
+        document.documentElement.style.setProperty('--bg-color', '#a6a0cf');
+    }
 }
 
 // Step 3: Register a 'click' event listener
@@ -76,9 +82,60 @@ const addParticipant = (event) => {
     participantsDiv.appendChild(newCount);
 }
 
-// Step 3: Add a click event listener to the submit RSVP button
-rsvpButton.addEventListener('click', addParticipant);
+/*** Form Validation ***
+  
+  Purpose:
+  - Prevents invalid form submissions from being added to the list of participants.
 
-/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
+  When To Modify:
+  - [ ] Project 7 (REQUIRED FEATURE)
+  - [ ] Project 7 (STRETCH FEATURE)
+  - [ ] Project 9 (REQUIRED FEATURE)
+  - [ ] Any time between / after
+***/
+
+// Step 1: We actually don't need to select the form button again -- we already did it in the RSVP code above.
+
+// Step 2: Write the callback function
+const validateForm = (event) => {
+    
+    let containsErrors = false;
+    
+    var rsvpInputs = document.getElementById("rsvp-form").elements;
+    
+    // Loop through all inputs to validate
+    for (let i = 0; i < rsvpInputs.length; i++) {
+        // Check if the input value is less than 2 characters
+        if (rsvpInputs[i].value.length < 2) {
+            containsErrors = true;
+            rsvpInputs[i].classList.add('error');
+        } else {
+            rsvpInputs[i].classList.remove('error');
+        }
+    }
+    
+    // Validate email for @ symbol
+    const email = document.getElementById('email');
+    if (!email.value.includes('@')) {
+        containsErrors = true;
+        email.classList.add('error');
+    } else {
+        email.classList.remove('error');
+    }
+    
+    // If no errors, call addParticipant() and clear fields
+    if (containsErrors == false) {
+        addParticipant(event);
+        // Clear form inputs
+        for (let i = 0; i < rsvpInputs.length; i++) {
+            rsvpInputs[i].value = "";
+        }
+    }
+    
+}
+
+// Step 3: Register a 'click' event listener
+rsvpButton.addEventListener('click', validateForm);
+
 /*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
 /*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
